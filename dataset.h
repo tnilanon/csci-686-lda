4930483af5cb434c1194b7391c9cf8248a3be1f2
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2015-10-08 02:21:09 -0700

    Add header file to store public data
    Signed-off-by: Tanachat Nilanon <tnilanon@users.noreply.github.com>

---
+++ dataset.h
@@ -0,0 +1,9 @@
+#ifndef FAST_LDA_DATASET_H_
+#define FAST_LDA_DATASET_H_
+
+unsigned long num_iterations, num_topics;
+unsigned long D, W, NNZ;
+unsigned long * size_i, ** word_i_j, ** count_i_j;
+
+#endif  // FAST_LDA_DATASET_H_
+
